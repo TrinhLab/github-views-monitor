57ad0bfb93720d45657e98afd27348a4fae87757
Sergio Garcia <sergiogarciamf@gmail.com>
2019-10-11 12:32:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Usage monitor
+
+Monitor views of software distributed through Github.
+The metric is views (individuals visiting the repository page) which takes into account unique individuals.
+Currently the total number of downloads cannot be monitored. Only release download and clones, but not zip downloads.
+
+The data is stored in a table named data.tsv with the following headers:
+- repo_name: Name of repository
+- log_timestamp: Time the request was done by us
+- github_timestamp: Time github recorded the data
+- count: Total number of visits
+- unique: Total number of unique visitors
+
+
+# Running
+
+Requires credentials for github api:
+https://developer.github.com/v3/#authentication
+You can generate an authentication token here (needs push permission, which can be granted with public_repo scope):
+https://github.com/settings/tokens
+
+run `./log.py -h` for more info
+
+Set up as cronjob to run regularly, for example to run once a week (Sunday at midnight to be specific):
+0 0 * * 0 /path/to/usage-monitor/log.py -o /path/to/usage-monitor/data.tsv -t /path/to/usage-monitor/secret_token
+
+# Notes
+Currently it is just monitoring the data, after sufficient data is available a function to plot it will be added
---
+++ log.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import requests
+import datetime
+import argparse
+
+def main():
+    #arguments
+    parser = argparse.ArgumentParser(description='Log github views of public repositories for an organization',     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-o', '--data_file_path', type=str, help='Path to .tsv file where data is stored', default="data.tsv")
+    parser.add_argument('-t', '--secret_token_path', type=str, help='Path to one line text file with authentication token', default="secret_token")
+    parser.add_argument('-v', '--verbose', action='store_true', help='Turn on verbose output')
+    parser.add_argument('-n', '--org_name', default="trinhlab", help='Organization name')
+
+    args = parser.parse_args()
+
+    # program
+
+    # Get repo names
+    resp = requests.get(f'https://api.github.com/orgs/{args.org_name}/repos')
+    repo_names = [repo['name'] for repo in resp.json()]
+
+
+    # Get traffic
+    with open(args.secret_token_path, "r") as f:
+        secret_token = f.read().splitlines()[0]
+
+    def write_row():
+        if args.verbose:
+            print("\t".join([name, log_timestamp, github_timestamp, count, uniques]))
+        f.write("{}\n".format("\t".join([name, log_timestamp, github_timestamp, count, uniques])))
+
+    headers = { "Authorization": f"token {secret_token}"}
+    parameters = {"per": "week"}
+    log_timestamp = datetime.datetime.now().isoformat().split(".")[0] #remove miliseconds
+    with open(args.data_file_path, 'a') as f:
+        for name in repo_names:
+            # Github returns the last two weeks
+            resp = requests.get(f"https://api.github.com/repos/{args.org_name}/{name}/traffic/views", headers=headers, params=parameters)
+            info = resp.json()
+
+            # Table headers:
+            # repo_name | log_timestamp | github_timestamp | count | unique
+            if not info['views']:
+                github_timestamp =''
+                count = '0'
+                uniques = '0'
+                write_row()
+            else:
+                for week in info['views']:
+                    github_timestamp = week['timestamp']
+                    count = str(week['count'])
+                    uniques = str(week['uniques'])
+                    write_row()
+
+
+if __name__ == '__main__':
+   main()
